df262fa5470a85666edf7d990a0112fedecc055f
srinathmahto <srinath.mahto@gmail.com>
2026-04-06 13:41:20 +0530

    Delete adf-onprem-to-adls-pipeline directory

--- adf-onprem-to-adls-pipeline/sql-scripts/full_load.sql
+++
@@ -1,6 +0,0 @@
--- Query used dynamically in ADF for Full Load
--- Pipeline passes SchemaName and TableName dynamically
-
-SELECT * 
-FROM 
-    [@{pipeline().parameters.SchemaName}].[@{pipeline().parameters.TableName}]
--- adf-onprem-to-adls-pipeline/sql-scripts/incremental_load.sql
+++
@@ -1,8 +0,0 @@
--- Query used dynamically in ADF for Incremental Load
--- Pipeline passes SchemaName, TableName, WatermarkColumn, and LastWatermarkValue dynamically
-
-SELECT * 
-FROM 
-    [@{pipeline().parameters.SchemaName}].[@{pipeline().parameters.TableName}]
-WHERE 
-    [@{pipeline().parameters.WatermarkColumn}] > '@{pipeline().parameters.LastWatermarkValue}'
